c183f3cc520b4322395c17a632a3504e492c7616
Ümit UZ <umituz998@gmail.com>
2019-12-15 16:07:06 +0300

    declare a variable

--- main.go
+++ main.go
@@ -6,6 +6,29 @@ import (
 )
 
 func main() {
+	/**
+	@todo I am Ümit UZ and awesome!
+	 */
 	fmt.Println("Hello World Via GoLang")
+	/*
+	@todo math kütüphanesinden Pi fonksyionu çağırma işlemi gerçekleştirilir ve ekrana yazar
+	 */
 	fmt.Println(math.Pi)
+
+	/**
+	@todo fmt.Println iki değer döndürür. Biri int diğeri error.
+	* integer olan bite sayısıdır
+	* error da <nil> olarak döner, yani nothing anlamındadır
+	 */
+	message, error := fmt.Println("Hi There")
+	fmt.Println(message)
+	fmt.Println(error)
+
+	/**
+	@todo Dönen değerleri değişkenlere aktarmak gerekir ama kullanılmazsa uyar verir.
+	* golang de bir değişken kullanılmaz ise hata verir, yani değişken tanımlanmış ise kullanılmak zorundadır.
+	* Bunun için kullanılmayacak olan değişkeni _ işareti ile belirtmemiz gerekir.
+	*/
+	x, _ := fmt.Println("Hello")
+	fmt.Println(x)
 }
