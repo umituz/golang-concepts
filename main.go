476f12fb623dbc1e624c388d5159e18e7a08dd1d
Ümit UZ <umituz998@gmail.com>
2021-02-28 23:31:42 +0300

    short declaration for variables

--- main.go
+++ main.go
@@ -2,44 +2,58 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	/**
 	@todo I am Ümit UZ and awesome! :D
 	*/
-	fmt.Println("Hello World Via GoLang")
+
+	//fmt.Println("Hello World Via GoLang")
+
 	/*
 		@todo math kütüphanesinden Pi fonksyionu çağırma işlemi gerçekleştirilir ve ekrana yazar
 	*/
-	fmt.Println(math.Pi)
+
+	//fmt.Println(math.Pi)
 
 	/**
 	@todo fmt.Println iki değer döndürür. Biri int diğeri error.
 	* integer olan bite sayısıdır
 	* error da <nil> olarak döner, yani nothing anlamındadır
 	*/
-	message, error := fmt.Println("Hi There")
-	fmt.Println(message)
-	fmt.Println(error)
+
+	//message, error := fmt.Println("Hi There")
+	//fmt.Println(message)
+	//fmt.Println(error)
 
 	/**
 	@todo Dönen değerleri değişkenlere aktarmak gerekir ama kullanılmazsa uyar verir.
 	* golang de bir değişken kullanılmaz ise hata verir, yani değişken tanımlanmış ise kullanılmak zorundadır.
 	* Bunun için kullanılmayacak olan değişkeni _ işareti ile belirtmemiz gerekir.
 	*/
-	x, _ := fmt.Println("Hello")
-	fmt.Println(x)
+
+	//x, _ := fmt.Println("Hello")
+	//fmt.Println(x)
 
 	/**
 	* Değişken tanımlamak için var anahtar kelimesi kullanılır.
 	 */
-	var language = "Go Lang"
-	fmt.Println(language)
-	var name, surname = "Ümit", "UZ"
-	fmt.Println(name, surname)
-	var dream string
-	dream = "Change The World"
-	fmt.Println(dream)
+
+	//var language = "Go Lang"
+	//fmt.Println(language)
+	//var name, surname = "Ümit", "UZ"
+	//fmt.Println(name, surname)
+	//var dream string
+	//dream = "Change The World"
+	//fmt.Println(dream)
+
+	/**
+	* Kısa değişken tanımlamalarını döngü içerisinde kullanılabilir.
+	* var ile kullanılamaz
+	 */
+	hey := 15
+	for i := 0; i <= hey; i++ {
+		fmt.Println(i)
+	}
 }
