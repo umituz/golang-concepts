9cf5cfbf205806e415238885bdf7d696e82493f3
Umit Kenan <umitkenan@isider.umit.kenan>
2021-11-10 20:59:00 +0300

    array slices

---
+++ src/github.com/umituz/crash_course/05_arrays_slices/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var fruitArray [2]string
+	fruitArray[0] = "Apple"
+	fruitArray[1] = "Orange"
+	fmt.Println(fruitArray)
+	fmt.Println(fruitArray[0])
+	fmt.Println(fruitArray[1])
+
+	carArr := [2]string{"Lamborghini", "BMW"}
+	fmt.Println(carArr)
+	fmt.Println(carArr[0])
+	fmt.Println(carArr[1])
+
+	fruitSlice := []string{"Apple", "Orange", "Grape","Cherry"}
+	fmt.Println(fruitSlice)
+	fmt.Println(fruitSlice[0])
+	fmt.Println(fruitSlice[1])
+	fmt.Println(fruitSlice[2])
+	
+	fmt.Println(len(fruitSlice))
+	fmt.Println(fruitSlice[1:3])
+
+}
