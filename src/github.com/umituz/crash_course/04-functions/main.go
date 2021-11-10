109583ee19044dc5fb080c4a9ea8adde89160acb
Umit Kenan <umitkenan@isider.umit.kenan>
2021-11-10 20:47:39 +0300

    functions

---
+++ src/github.com/umituz/crash_course/04-functions/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func greeting(name string) string {
+	return "Hello " + name
+}
+
+func getSum (num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func main() {
+
+	fmt.Println(greeting("Man"))
+	fmt.Println(getSum(10,10))
+
+}
