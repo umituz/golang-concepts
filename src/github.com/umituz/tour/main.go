054d333d6bc693a3733f46204c3d440ce00c79c1
Ümit Kenan UZ <umituz998@gmail.com>
2021-11-15 23:15:17 +0300

    .

--- src/github.com/umituz/tour/main.go
+++ src/github.com/umituz/tour/main.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"math/cmplx"
 )
 
-var c, python, java bool
-var k, j int = 1, 2
+const Pi = 3.14
 
 func main() {
 
@@ -23,6 +23,27 @@ func main() {
 	fmt.Println(i, c, python, java)
 	var c2, python2, java2 = true, false, "no!"
 	fmt.Println(k, j, c2, python2, java2)
+
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+	var ii int
+	var f float64
+	var bb bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", ii, f, bb, s)
+	var x, y int = 3, 4
+	var ff float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(ff)
+	fmt.Println(x, y, z)
+	v := 42 // change me!
+	fmt.Printf("v is of type %T\n", v)
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
 }
 
 func add(x int, y int) int {
@@ -42,3 +63,12 @@ func split(sum int) (x, y int) {
 	y = sum - x
 	return
 }
+
+var c, python, java bool
+var k, j int = 1, 2
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
