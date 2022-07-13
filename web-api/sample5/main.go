eabccb0daddea1deec99ab82d13a9fd4a34f1622
Ümit Kenan UZ <umituz998@gmail.com>
2022-07-13 08:31:51 +0300

    web api sample 5

---
+++ web-api/sample5/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+var homepage = "index.html"
+
+func loadFile(fileName string) (string, error) {
+	bytes, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var body, _ = loadFile(homepage)
+	fmt.Fprintf(w, body)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9000", nil)
+}
